4483b1d9ad856ac332916f11c11650b55e298214
Kwangsu-CHOI <wisecks@gmail.com>
2023-01-17 15:27:04 +1100

    update server url

--- client/main.js
+++ client/main.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch bot's response from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://askme-0hkd.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
